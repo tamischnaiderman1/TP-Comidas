97957af659fc760dba35136b3af4f4993e5c06e1
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-01 11:53:26 -0300

    alert hecho

---
+++ Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import {
+StyleSheet,
+Text,
+View,
+Image,
+TextInput,
+Button,
+TouchableOpacity,
+} from "react-native";
--- Login.js
+++ Login.js
@@ -19,55 +19,74 @@ return (
     <StatusBar style="auto" />
     <TextInput
         style={styles.TextInput}
+        value={email}
         placeholder="Email"
         placeholderTextColor="#000"
         onChangeText={(email) => setEmail(email)}
     />
     <TextInput
         style={styles.TextInput}
+        value={password}
         placeholder="Password"
         placeholderTextColor="#000"
         secureTextEntry={true}
         onChangeText={(password) => setPassword(password)}
     />
-    <TouchableOpacity style={styles.loginBtn}>
+    <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => {
+        if (email == "" || password == "") {
+            window.alert("Por favor, complete ambos campos.");
+        } else {
+        }
+        }}
+    >
         <Text style={styles.loginText}>LOGIN</Text>
     </TouchableOpacity>
     </View>
 );
+}
 
-}const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    inputView: {
-        backgroundColor: "#FFC0CB",
-        width:"70%",
-        height: 45,
-        marginBottom: 20,
-        alignItems: "center",
-    },
-    TextInput: {
-        borderradius: 30,
-        borderColor:"#a8977d",
-        height: 50,
-        padding: 10,
-        marginBottom: 10,
-        alignItems: "center",
-        marginLeft: 20,
-    },
-    loginBtn: {
-        width: "80%",
-        borderRadius: 25,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 40,
-        backgroundColor: "#a8977d",
-        marginBottom: 50,
-    }
-})
+export const post = async (data) => {
+return client
+    .post("", { ...data })
+    .then((response) => response.data)
+    .catch((error) => {
+    console.log(error);
+    throw error;
+    });
+};
 
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+},
+inputView: {
+    backgroundColor: "#FFC0CB",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+},
+TextInput: {
+    borderRadius: 30,
+    borderColor: "#a8977d",
+    height: 50,
+    padding: 10,
+    marginBottom: 10,
+    alignItems: "center",
+    width: "80%",
+},
+loginBtn: {
+    width: "80%",
+    borderRadius: 30,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#a8977d",
+    marginBottom: 50,
+},
+});
